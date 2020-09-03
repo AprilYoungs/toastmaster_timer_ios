3a8898c34da5aa595bf35799bcb6a07c1ee411f7
April Yang <yangchangjun010@hotmail.com>
2020-09-03 22:25:41 +0800

    Initial Commit

---
+++ ToastmasterTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToastmasterTimer
+//
+//  Created by April Yang on 2020/9/3.
+//  Copyright © 2020 April Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
