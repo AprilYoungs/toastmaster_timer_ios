ba9345aca287b961c09a6b0b747aa91e50a57d1a
April <yangchangjun010@hotmail.com>
2021-02-06 10:24:01 +0800

    feat: set highlight color for timeliest

--- ToastmasterTimer/RecordItemCell.swift
+++ ToastmasterTimer/RecordItemCell.swift
@@ -11,8 +11,8 @@ import UIKit
 class RecordItemCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
-     @IBOutlet weak var totalLabel: UILabel!
-     @IBOutlet weak var usedLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var usedLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         self.selectionStyle = .none
@@ -22,11 +22,15 @@ class RecordItemCell: UITableViewCell {
         self.nameLabel.text = recordItem.name
         self.totalLabel.text = formatTimeString(second: recordItem.totalTime)
         self.usedLabel.text = formatTimeString(second: recordItem.usedTime)
+        self.usedLabel.textColor = recordItem.usedTime - recordItem.totalTime > 15 ? UIColor.systemRed :
+            recordItem.usedTime - recordItem.totalTime < -15 ? UIColor.systemGreen :
+            UIColor.darkText
     }
     
     func setAsHeader() {
         self.nameLabel.text = "name"
         self.totalLabel.text = "total"
         self.usedLabel.text = "used"
+        self.usedLabel.textColor = UIColor.darkText
     }
 }
